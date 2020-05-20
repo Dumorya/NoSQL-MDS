68a2d767885ff5a876a7ae7d21fe1444b7b9ed17
Unknown <clara.tiare@live.fr>
2020-05-20 14:07:03 +0200

    fin étape 2

--- src/main/java/com/cassel/nosql/App.java
+++ src/main/java/com/cassel/nosql/App.java
@@ -4,11 +4,17 @@
 package com.cassel.nosql;
 
 import java.util.Arrays;
+import java.util.Scanner;
+
+import org.bson.Document;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
 import com.mongodb.client.MongoClient;
 
 public class App {
@@ -63,13 +69,84 @@ public class App {
             }
         }
         
-        getMongoClient(host, port);
+        // connection
+        MongoClient mongoClient = getMongoClient(host, port);
         
-        /*MongoDatabase database = mongoClient.getDatabase("mydb");
-        MongoCollection<Document> collection = database.getCollection("test");
+        // get db
+        MongoDatabase db = mongoClient.getDatabase(database);
+        
+        // get collections name 
+        MongoIterable <String> collections = db.listCollectionNames();
+        
+        for (String collectionName: collections)
+        {
+            System.out.println(collectionName);
+        }
+        
+        boolean chosen = false;
+        String chosenCollection = "";
+        
+    	Scanner sc = new Scanner(System.in);
 
-        Document myDoc = collection.find();
-        System.out.println(myDoc.toJson());*/
+        while(!chosen)
+        {
+            System.out.println("Veuillez saisir une collection, ou 'fin' pour finir :");
+            chosenCollection = sc.nextLine();
+            
+            for (String collectionName: collections)
+            {
+                if(chosenCollection.equals(collectionName))
+                {
+                    System.out.println("Vous avez saisi : " + chosenCollection);
+                    chosen = true;
+                }
+                else if(chosenCollection.equals("fin"))
+                {
+                	System.out.println("FIN");
+                	chosen = true;
+                }
+                else
+                {
+                	System.out.println("Cette collection n'existe pas.");
+                	chosen = false;
+                }
+            }
+        }
+        
+        MongoCollection<Document> collection = db.getCollection(chosenCollection);
+        
+        chosen = false;
+        
+        while(!chosen)
+        {
+            System.out.println("Taper 're' pour rechercher un document, 'in' pour insérer un document, 'fin' pour arrêter.");
+            String choice = sc.nextLine();
+            
+            	if(choice.equals("re"))
+                {
+                    System.out.println("Vous avez choisi de rechercher un document");
+                    chosen = true;
+                }
+                else if(chosenCollection.equals("in"))
+                {
+                	System.out.println("Vous avez choisi d'insérer un document");
+                	chosen = true;
+                }
+                else if(chosenCollection.equals("fin"))
+                {
+                	System.out.println("FIN");
+                	chosen = true;
+                }
+                else
+                {
+                	System.out.println("Ce choix n'existe pas. Recommencer");
+                	chosen = false;
+                }
+
+        }
+        
+        // Document myDoc = collection.find();
+        // System.out.println(myDoc.toJson());
     }
     
     public static MongoClient getMongoClient(final String host, final int port)
@@ -79,7 +156,7 @@ public class App {
                         .applyToClusterSettings(builder ->
                                 builder.hosts(Arrays.asList(new ServerAddress(host, port))))
                         .build());
-    	
+    	    	    	
     	return mongoClient;
     }
 }
