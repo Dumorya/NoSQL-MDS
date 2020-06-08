5b2b47d87a998a7ece4c0f7f9b2346e75f6255d4
Unknown <clara.tiare@live.fr>
2020-06-08 23:22:48 +0200

    étape 5 finie

--- src/main/java/com/cassel/nosql/UserInteractions.java
+++ src/main/java/com/cassel/nosql/UserInteractions.java
@@ -11,6 +11,7 @@ import org.bson.BsonDocument;
 import org.bson.BsonInt32;
 import org.bson.BsonString;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
@@ -20,6 +21,10 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Updates.*;
 
 public class UserInteractions
 {
@@ -90,6 +95,7 @@ public class UserInteractions
             else if(choice.equals("in"))
             {
             	System.out.println("Vous avez choisi d'insérer un document");
+            	insertDocument(collection);
             	chosen = true;
             }
             else if(choice.equals("fin"))
@@ -115,6 +121,149 @@ public class UserInteractions
 		
 		System.out.println("Veuillez taper le nom d'un des champs qui s'affichent ci-dessous :");
 		
+		displayFields(documents, availableKeys, fieldsType);
+		
+		Scanner sc = Singleton.getInstance().getScanner();
+		
+		boolean chosen = false;
+		
+		// choose the field
+		String chosenField = chooseField(availableKeys, sc, chosen);
+                
+        // choose the operator
+        String operator = chooseOperator(sc);
+    	
+    	// get data to compare
+    	String dataToCompare = chooseDataToCompare(sc);
+    	
+    	// display search results
+    	FindIterable<Document> searchedDocuments = getSearchResults(collection, fieldsType, chosenField, operator, dataToCompare);
+    	
+    	// get query
+    	BsonDocument querySearchResults = buildSearchQuery(fieldsType, chosenField, operator, dataToCompare);
+    	
+    	// choose action after displaying results
+    	System.out.println("Que souhaitez-vous faire ? \n"
+    			+ "1 : supprimer les documents récupérés \n"
+    			+ "2 : supprimer un des documents récupérés \n"
+    			+ "3 : modifier les documents récupérés \n"
+    			+ "4 : modifier un document parmis les documents récupérés \n");
+    	
+    	String chosenOptionAfterSearchDisplayed = sc.nextLine();
+    	
+    	if(chosenOptionAfterSearchDisplayed.contentEquals("1"))
+    	{
+    		deleteManyAfterSearch(collection, querySearchResults);
+    	}
+    	else if(chosenOptionAfterSearchDisplayed.contentEquals("2"))
+    	{
+    		deleteOneAfterSearch(collection, querySearchResults);
+    	}
+    	else if(chosenOptionAfterSearchDisplayed.contentEquals("3"))
+    	{
+    		editDocuments(collection, fieldsType, sc, searchedDocuments);
+    	}
+    	else if(chosenOptionAfterSearchDisplayed.contentEquals("4"))
+    	{
+    		editDocument(collection, sc, searchedDocuments);
+    	}
+    }
+	
+	
+	public void insertDocument(MongoCollection<Document> collection)
+	{	
+		Document obj = new Document();
+		FindIterable<Document> documents = collection.find();
+		ArrayList<String> availableKeys = new ArrayList<>();
+		Map<String, String> fieldsType = new HashMap<>();
+		ArrayList<String> fields = new ArrayList<>();
+		
+		Scanner sc = Singleton.getInstance().getScanner();
+		
+		fields = getFields(documents, availableKeys, fieldsType);
+		
+		for(String field : fields)
+		{
+			System.out.printf("Insérer une valeur pour le champ %s, rentrer 'null' pour ne pas renseigner le champ.", field);
+						
+			String valueToInsert = sc.nextLine();
+			
+			if(valueToInsert != "null")
+			{
+				obj.put(field, valueToInsert);
+			}
+		}
+		
+		collection.insertOne(obj);
+    }
+
+	
+	private void editDocument(MongoCollection<Document> collection, Scanner sc,
+			FindIterable<Document> searchedDocuments)
+	{
+		Bson filter = null;
+		Bson query = null;
+		ArrayList<String> availableKeysEditMany = new ArrayList<>();
+		Map<String, String> fieldsTypeEditMany = new HashMap<>();
+		
+		System.out.println("Quel document voulez-vous modifier ? Entrez son ID pour le modifier");
+		
+		String chosenId = sc.nextLine();
+		
+		filter = eq("_id", chosenId);
+		
+		System.out.println("Quel champs souhaitez-vous modifier ?");
+		
+		// display fields
+		displayFields(searchedDocuments, availableKeysEditMany, fieldsTypeEditMany);
+
+		String chosenFieldAfterSearchDisplayed = sc.nextLine();
+		
+		// get data to edit
+		String dataToEdit = chooseDataToEdit(sc);
+		
+		query = combine(set(chosenFieldAfterSearchDisplayed, dataToEdit));
+		
+		UpdateResult result = collection.updateOne(filter, query);
+		
+		System.out.println("Nombre d'enregistrements modifiés : " + result.getModifiedCount());
+	}
+
+	
+	private void editDocuments(MongoCollection<Document> collection, Map<String, String> fieldsType, Scanner sc,
+			FindIterable<Document> searchedDocuments)
+	{
+		ArrayList<String> availableKeysEditMany = new ArrayList<>();
+		Map<String, String> fieldsTypeEditMany = new HashMap<>();
+		    		
+		System.out.println("Quel champs souhaitez-vous modifier ?");
+		
+		// display fields
+		displayFields(searchedDocuments, availableKeysEditMany, fieldsTypeEditMany);
+
+		String chosenFieldAfterSearchDisplayed = sc.nextLine();
+		
+		// get data to edit
+		String dataToEdit = chooseDataToEdit(sc);
+		    		
+		searchedDocuments.forEach((doc) -> collection.updateOne(
+		    eq("_id", doc.get("_id")), set(chosenFieldAfterSearchDisplayed, getValue(fieldsType, chosenFieldAfterSearchDisplayed, dataToEdit)))
+		);
+		
+		System.out.println("Les champs ont été modifiés !");
+	}
+
+	
+	private void deleteOneAfterSearch(MongoCollection<Document> collection, BsonDocument querySearchResults)
+	{
+		collection.deleteOne(querySearchResults);
+		System.out.printf("Un document a été supprimé.");
+	}
+
+	
+	private void displayFields(FindIterable<Document> documents, ArrayList<String> availableKeys,
+			Map<String, String> fieldsType)
+	{
 		for(Document doc : documents)
 		{
 			for(String key : doc.keySet())
@@ -137,37 +286,145 @@ public class UserInteractions
 				}
 			}
 		}
+	}
+	
+	
+	private ArrayList<String> getFields(FindIterable<Document> documents, ArrayList<String> availableKeys,
+			Map<String, String> fieldsType)
+	{
+		for(Document doc : documents)
+		{
+			for(String key : doc.keySet())
+			{
+				if(!availableKeys.contains(key))
+				{
+					availableKeys.add(key);
+					
+					String typeString = doc.get(key) == null ? "null" : doc.get(key).getClass().getName();
+			        if (!typeString.equals(ArrayList.class.getName()))
+			        {
+			          fieldsType.put(key, typeString);
+			        }
+			        else
+			        {
+			          // System.out.println("Array");
+			        }
+			        
+				}
+			}
+		}
 		
-		Scanner sc = Singleton.getInstance().getScanner();
-		
-		boolean chosen = false;
-		String chosenField = "";
+		return availableKeys;
+	}
 
-		// choose the field
-        while(!chosen)
+	private void deleteManyAfterSearch(MongoCollection<Document> collection, BsonDocument querySearchResults)
+	{
+		DeleteResult deleteResults = collection.deleteMany(querySearchResults);
+		System.out.printf("Nombre de documents supprimés : %s", deleteResults.getDeletedCount());
+	}
+
+	
+	private FindIterable<Document> getSearchResults(MongoCollection<Document> collection, Map<String, String> fieldsType,
+			String chosenField, String operator, String dataToCompare)
+	{
+		BsonDocument query = buildSearchQuery(fieldsType, chosenField, operator, dataToCompare);
+    	
+    	FindIterable<Document> results = collection.find(query);
+    	
+    	System.out.println("Recherche en cours...");
+    	
+    	for(Document result : results)
+		{
+			System.out.println(result.toJson());
+		}
+    	
+    	return results;
+	}
+	
+	
+	public Object getValue(Map<String, String> fieldsType, String field, String value)
+	{
+        Class<?> type;
+        
+        try
         {
-			chosenField = sc.nextLine();
-
-			if(availableKeys.contains(chosenField))
-			{
-				System.out.printf("Vous avez choisi %s.", chosenField);
-				chosen = true;
-			}
-			else
-			{
-				System.out.println("Le champ choisi n'existe pas. Veuillez réessayer.");
-				chosen = false;
-			}
+            type = Class.forName(fieldsType.get(field));
+        }
+        catch(ClassNotFoundException e)
+        {
+            e.printStackTrace();
+            type = String.class;
         }
         
-        //TODO: get datatype, in order to check automatically if the operator is correct
+        if(type == Integer.class)
+        {
+            return Integer.parseInt(value);
+        }
+        else
+        {
+            return value;
+        }
+	}
+
+	private BsonDocument buildSearchQuery(Map<String, String> fieldsType, String chosenField, String operator,
+			String dataToCompare)
+	{
+		Class<?> cls = null;
+        try
+        {
+        	cls = Class.forName(fieldsType.get(chosenField));
+        }
+        catch (ClassNotFoundException e)
+        {
+        	e.printStackTrace();
+        	cls = String.class;
+        }
         
-        // choose the operator
-        
-        chosen = false;
+    	BsonDocument query = null;
+    	    	
+    	if(cls == String.class)
+    	{
+    		query = new BsonDocument(chosenField, new BsonDocument(operator, new BsonString(dataToCompare)));
+    	}
+    	else if(cls == Integer.class)
+    	{
+    		int dataInt = Integer.parseInt(dataToCompare);
+    		query = new BsonDocument(chosenField, new BsonDocument(operator, new BsonInt32(dataInt)));
+    	}
+    	else if(cls == Boolean.class)
+    	{
+    		boolean dataBool = Boolean.parseBoolean(dataToCompare);
+    		query = new BsonDocument(chosenField, new BsonDocument(operator, new BsonBoolean(dataBool)));
+    	}
+    	
+		return query;
+	}
+
+	private String chooseDataToCompare(Scanner sc)
+	{
+		System.out.println("Quelle est la valeur avec laquelle vous souhaitez comparer ?");
+    	
+    	String dataToCompare = "";
+
+    	dataToCompare = sc.nextLine();
+		return dataToCompare;
+	}
+	
+	private String chooseDataToEdit(Scanner sc)
+	{
+		System.out.println("Quelle est la valeur que vous souhaitez insérer ?");
+    	
+    	String dataToEdit = "";
+
+    	dataToEdit = sc.nextLine();
+		return dataToEdit;
+	}
+
+	private String chooseOperator(Scanner sc)
+	{
 		String chosenOperator = "";
 		
-		String operators [] = {"inf", "inf egal", "sup", "sup egal", "string egal", "bool egal", "bool non egal"};
+		String operators [] = {"inf", "inf egal", "sup", "sup egal", "egal", "bool egal", "bool non egal"};
 		
 		System.out.println("Veuillez choisir un opérateur parmis ceux ci-dessous :");
 		
@@ -209,62 +466,39 @@ public class UserInteractions
     	
     	if(chosenOperator.equals("bool egal"))
     	{
-    		operator = "=";
+    		operator = "$eq";
     	}
     	
     	if(chosenOperator.equals("bool non egal"))
     	{
-    		operator = "!=";
+    		operator = "$ne";
     	}
     	
-    	
-    	// get data to compare
-    	System.out.println("Quelle est la valeur avec laquelle vous souhaitez comparer ?");
-    	
-    	String dataToCompare = "";
+		return operator;
+	}
 
-    	dataToCompare = sc.nextLine();
-    	
-    	Class<?> cls = null;
-        try
+	
+	private String chooseField(ArrayList<String> availableKeys, Scanner sc, boolean chosen)
+	{
+		String chosenField = "";
+
+		// choose the field
+        while(!chosen)
         {
-        	cls = Class.forName(fieldsType.get(chosenField));
-        }
-        catch (ClassNotFoundException e)
-        {
-        	e.printStackTrace();
-        	cls = String.class;
+			chosenField = sc.nextLine();
+
+			if(availableKeys.contains(chosenField))
+			{
+				System.out.printf("Vous avez choisi %s.", chosenField);
+				chosen = true;
+			}
+			else
+			{
+				System.out.println("Le champ choisi n'existe pas. Veuillez réessayer.");
+				chosen = false;
+			}
         }
         
-    	BsonDocument query = null;
-    	    	
-    	if(cls == String.class)
-    	{
-    		query = new BsonDocument(chosenField, new BsonDocument(operator, new BsonString(dataToCompare)));
-    	}
-    	else if(cls == Integer.class)
-    	{
-    		int dataInt = Integer.parseInt(dataToCompare);
-    		query = new BsonDocument(chosenField, new BsonDocument(operator, new BsonInt32(dataInt)));
-    	}
-    	else if(cls == Boolean.class)
-    	{
-    		boolean dataBool = Boolean.parseBoolean(dataToCompare);
-    		query = new BsonDocument(chosenField, new BsonDocument(operator, new BsonBoolean(dataBool)));
-    	}
-    	
-    	FindIterable<Document> results = collection.find(query);
-    	
-    	System.out.println("Recherche en cours...");
-    	
-    	for(Document result : results)
-		{
-			System.out.println(result);
-		}
-    }
-	
-	public void insertDocument()
-	{
-    	
-    }
+		return chosenField;
+	}
 }
